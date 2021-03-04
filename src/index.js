3c53aab358f041fa1cbd974831ea6cd004041e94
RajPotla <potla001@cougars.csusm.edu>
2021-03-04 12:46:29 -0600

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,93 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './App.css';
+import data from './data.json';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filterOption: '',
+      filteredData: []
+    };
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  optionSelected(e) {
+    this.setState({
+      filterOption: e.target.value
+    })
+  }
+
+  renderTableData(data) {
+    return data.map((element, i) => (
+      <tr key={i}> 
+        {/* <td>{element.Name}</td> */}
+        <td>{element.Env}</td>
+        <td>{element.LOB}</td> 
+      </tr>
+    ))
+  }
+
+  renderFilterOptions(filterOption) {
+    if(filterOption) {
+      let options = Array.from(new Set(data.map(element => element[filterOption])));
+      return options.map((option,i) => <option key={i} value={option}> {option} </option>)
+    }
+    return []
+  }
+
+  filterOptionSelected(e) {
+    let {value} = e.target
+    if(value) {
+      let { filterOption } = this.state
+      let filteredData = data.filter(element => element[filterOption] === value);
+      this.setState({
+        filteredData: filteredData
+      })
+    }
+  }
+
+  render() {
+    let { filterOption, filteredData } = this.state
+    return (
+      <div>
+        <div>
+          <label> Filter By </label>
+          <select onChange={this.optionSelected.bind(this)}> 
+            <option value="">  </option>
+            <option value="Env"> Env </option>
+          </select>
+
+          <select onChange={this.filterOptionSelected.bind(this)}> 
+            <option value="">  </option>
+            {this.renderFilterOptions(filterOption)}
+          </select>
+
+          <label> Filter By </label>
+          <select onChange={this.optionSelected.bind(this)}> 
+            <option value="">  </option>
+            <option value="LOB"> LOB </option>
+          </select>
+          
+          <select onChange={this.filterOptionSelected.bind(this)}> 
+            <option value="">  </option>
+            {this.renderFilterOptions(filterOption)}
+          </select>
+        </div>
+        <table border="1">
+          <thead>
+            <th> Env </th>
+            <th> LOB </th>
+          </thead>
+          <tbody>
+             {
+               filteredData.length? this.renderTableData(filteredData) : this.renderTableData(data) 
+             }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
